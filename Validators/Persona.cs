f3df036ac7354f4579d5cf7b3307a2eadd905bc7
huanlui <juanluisgarcialopez@outlook.es>
2017-12-01 21:49:16 +0100

    First test

---
+++ Validators/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Validators
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public List<string> Coches { get; set; }
+
+
+    }
+}
---
+++ Validators/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Validators
+{
+    public static class Validator
+    {
+        public static bool NotNull(String value)
+        {
+            return !String.IsNullOrEmpty(value);
+        }
+
+        public static bool HasTwoCarsWithStartWithA(Persona persona)
+        {
+            return persona.Coches.Count(c => c.StartsWith("a")) == 2;
+        }
+    }
+}
